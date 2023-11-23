f99a6f1ca5b3a9254cf5642d8b9d80391237799e
mittelmanjournal <147964412+mittelmanjournal@users.noreply.github.com>
2023-11-23 11:44:56 -0800

    Current working iteration of WikiScrape
    Future functionality addition ideas: add a date range for the compiled timeline text document so that if you only care about things in the timeline from 1900-Present you can specify this and have an easier time collecting the data you actually want. Also add Britannica encyclopedia scrape, because the structure is similar to wikipedia: https://www.britannica.com/search?query=text+input+here then select the first appearing card's link to go to the desired page.

---
+++ WikiScrape/WikiScrape/src/Main/ScraperApp.java
@@ -0,0 +1,428 @@
+package Main;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ScraperApp {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	private JPanel formPanel;
+	private JTextField linkInput, keywordInput, outputPathInput;
+	private JComboBox<String> extractionType;
+	private JCheckBox getDatesToggle;
+	
+	private JCheckBox includeCompiledSortedTimelineToggle;
+	
+	private JButton startButton;
+	private JScrollPane scrollPane;
+	
+	String links;
+	String keywords;
+	String extractionTypeSelected;
+	String outputPath;
+	boolean getDates;
+	
+	private ArrayList<String> stringsToBeSortedByDate;
+	FileWriter compiledSortedTimelineWriter;
+	boolean includeCompiledSortedTimeline;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScraperApp window = new ScraperApp();
+					window.frame.pack();
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ScraperApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		try {
+			UIManager.setLookAndFeel(new SubstanceGraphiteLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
+		frame.setTitle("Mittelman's Keyword & Date Scraper");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		ImageIcon icon = new ImageIcon("logo.png");
+		frame.setIconImage(icon.getImage());
+		frame.setPreferredSize(new Dimension(600, 300));
+		
+		formPanel = new JPanel(new GridBagLayout());
+		formPanel.setBackground(Color.WHITE); // Set background color for the form panel
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
+
+		// Labels
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		formPanel.add(new JLabel("List of Links (line separated, .txt file):"), gbc);
+
+		gbc.gridy++;
+		formPanel.add(new JLabel("List of Keywords (comma separated, .txt file):"), gbc);
+
+		gbc.gridy++;
+		formPanel.add(new JLabel("Extraction Type:"), gbc);
+
+		gbc.gridy++;
+		formPanel.add(new JLabel("Output Folder Path (ends with '\\'):"), gbc);
+
+		// Text Inputs
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		linkInput = new JTextField(20);
+		linkInput.setPreferredSize(new Dimension(300, 25));
+		formPanel.add(linkInput, gbc);
+
+		gbc.gridy++;
+		keywordInput = new JTextField(20);
+		keywordInput.setPreferredSize(new Dimension(300, 25));
+		formPanel.add(keywordInput, gbc);
+
+		gbc.gridy++;
+		String[] extractionTypes = { "Sentence", "Paragraph"};
+		extractionType = new JComboBox<>(extractionTypes);
+		extractionType.setPreferredSize(new Dimension(300, 25));
+		formPanel.add(extractionType, gbc);
+
+		gbc.gridy++;
+		outputPathInput = new JTextField(20);
+		outputPathInput.setPreferredSize(new Dimension(300, 25));
+		formPanel.add(outputPathInput, gbc);
+
+		// Toggle for Dates
+		gbc.gridx = 0;
+		gbc.gridy++;
+		gbc.gridwidth = 2;
+		getDatesToggle = new JCheckBox("Get Dates");
+		formPanel.add(getDatesToggle, gbc);
+
+		
+		gbc.gridx = 1;
+		includeCompiledSortedTimelineToggle = new JCheckBox("Include Sorted Timeline File");
+		formPanel.add(includeCompiledSortedTimelineToggle, gbc);
+		
+		// Button
+		gbc.gridy++;
+		gbc.gridx = 0;
+		gbc.gridwidth = 0;
+		startButton = new JButton("Start Scraping");
+		startButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startScraping();
+			}
+		});
+		formPanel.add(startButton, gbc);
+
+		scrollPane = new JScrollPane(formPanel);
+		frame.add(scrollPane, BorderLayout.CENTER);
+
+		
+	}
+	
+	private void startScraping() {
+		// Implement scraping logic here based on the input values
+		links = linkInput.getText();
+		keywords = keywordInput.getText();
+		extractionTypeSelected = (String) extractionType.getSelectedItem();
+		outputPath = outputPathInput.getText();
+		getDates = getDatesToggle.isSelected();
+
+		includeCompiledSortedTimeline = includeCompiledSortedTimelineToggle.isSelected();
+		if (includeCompiledSortedTimeline && getDates) {
+			stringsToBeSortedByDate = new ArrayList<String>();
+			try {
+				compiledSortedTimelineWriter = new FileWriter(outputPath + "CompiledSortedTimeline.txt");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		scrapeAndWriteEachLinkToFile(getLinksFromFile(links), outputPath, getKeywordsFromFile(keywords),
+				extractionTypeSelected, getDates, includeCompiledSortedTimeline, stringsToBeSortedByDate,
+				compiledSortedTimelineWriter);
+
+	}
+
+	public static void scrapeAndWriteEachLinkToFile(ArrayList<String> linksToScrape, String outputPath,
+			ArrayList<String> keywords, String extractionTypeSelected, boolean getDates,
+			boolean includeCompiledSortedTimeline, ArrayList<String> stringsToBeSortedByDate,
+			FileWriter compiledSortedTimelineWriter) {
+		// for every link, create a document that has each sentence (or other type) that
+		// contains any of the keywords in the document
+		for (int i = 0; i < linksToScrape.size(); i++) {
+			String writeText = "";
+			String url = "https://en.wikipedia.org/wiki/" + linksToScrape.get(i);
+			Document document = null;
+			try {
+				document = Jsoup.connect(url).get();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			Element heading = document.selectFirst("h1");
+
+			try (FileWriter textWriter = new FileWriter(outputPath + heading.text() + ".txt")) { //maybe instead of heading name of file be linksToScrape.get(i)
+				Element bodyContent = document.getElementById("bodyContent");
+				Element contentText = bodyContent.getElementById("mw-content-text");
+				Element contentTags = contentText.selectFirst("div.mw-content-ltr.mw-parser-output");
+				// Elements paragraphs = e2.select("p");
+				Element currentTag = contentTags.selectFirst("p");
+				while (currentTag != null) {
+					if (currentTag.tagName().equals("p")) {
+						for (String keyword : keywords) {
+							if (keyword != null && !keyword.equals("") && currentTag.text().contains(keyword)) {
+								if (extractionTypeSelected.equals("Sentence")) {
+									for (String sentence : getSentences(currentTag.text(), keyword)) {
+										if (!writeText.contains(sentence)) {
+											writeText += sentence + "\n\n";
+										}
+									}
+								} else if (extractionTypeSelected.equals("Paragraph")) {
+									if (!writeText.contains(currentTag.text())) {
+										writeText += currentTag.text() + "\n\n";
+									}
+								}
+							}
+						}
+
+						if (containsDateInformation(currentTag.text(), getDates)) {
+							// sentences
+							if (extractionTypeSelected.equals("Sentence")) {
+								for (String sentence : getSentences(currentTag.text())) {
+									if (!writeText.contains(sentence)) {
+										if(includeCompiledSortedTimeline) {
+											stringsToBeSortedByDate.add(url.replace("https://en.", "") + ": " + sentence);
+										}
+										writeText += sentence + "\n\n";
+									}
+								}
+							} // paragraphs
+							else if (extractionTypeSelected.equals("Paragraph")) {
+								if (!writeText.contains(currentTag.text())) {
+									if(includeCompiledSortedTimeline) {
+										stringsToBeSortedByDate.add(url.replace("https://en.", "") + ": " + currentTag.text());
+									}
+									writeText += currentTag.text() + "\n\n";
+								}
+							}
+
+						}
+
+					} // before this line, check if the text contains any of the keywords -> only if
+						// contained in the P, then find sentence it is in
+					currentTag = currentTag.nextElementSibling();
+				}
+				textWriter.write(writeText);
+				textWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//need to check getDates truthness too because if not true, and includeCST is true, we could be adding to a null arraylist and writing to a null filewriter.
+		if (includeCompiledSortedTimeline && getDates) {
+			try {
+				for (String s : sortYearsAndMonths(stringsToBeSortedByDate)) {
+					compiledSortedTimelineWriter.write(s + "\n\n");
+				}
+				compiledSortedTimelineWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static ArrayList<String> getLinksFromFile(String filePath) {
+		ArrayList<String> links = new ArrayList<String>();
+		String linksFilePath = filePath;
+
+		Path path = Paths.get(linksFilePath);
+
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+			if (Files.exists(path)) {
+				System.out.println("File exists at path: " + path.toAbsolutePath());
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					line = line.trim();
+					line = line.replace("https://en.wikipedia.org/wiki/", "");
+					links.add(line);
+				}
+
+			} else {
+				System.out.println("File does not exist at this path.");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return links;
+	}
+
+	public static ArrayList<String> getKeywordsFromFile(String keywordsFilePath) {
+		if (!keywordsFilePath.equals("") && keywordsFilePath != null) {
+			File file = new File(keywordsFilePath);
+
+			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+				StringBuilder content = new StringBuilder();
+				String line;
+
+				// Read the entire file into a string
+				while ((line = reader.readLine()) != null) {
+					content.append(line);
+				}
+
+				// Split the string by commas and store in an ArrayList
+				ArrayList<String> dataList = new ArrayList<String>(Arrays.asList(content.toString().split(",")));
+				for (int i = 0; i < dataList.size(); i++) {
+					dataList.set(i, dataList.get(i).trim());
+				}
+				for (int i = 0; i < dataList.size(); i++) {
+					if (dataList.get(i).equals("")) {
+						dataList.remove(i);
+						i--;
+					}
+				}
+
+				return dataList;
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return new ArrayList<String>();
+	}
+
+	public static ArrayList<String> getSentences(String paragraph) {
+		ArrayList<String> sentences = new ArrayList<String>();
+
+		// Split the paragraph into sentences using a simple regex
+		String[] sentenceArray = paragraph.split("[.!?]");
+
+		// Check each sentence for the presence of the keyword
+		for (String sentence : sentenceArray) {
+			if (containsDateInformation(sentence, true)) { // we already know its true because we only call this func from a place where it must be previously true to reach
+				sentences.add(sentence.trim()); // trim() removes leading and trailing whitespaces
+			}
+		}
+
+		return sentences;
+	}
+	
+	public static ArrayList<String> getSentences(String paragraph, String keyword) {
+		ArrayList<String> sentences = new ArrayList<String>();
+
+		// Split the paragraph into sentences using a simple regex
+		String[] sentenceArray = paragraph.split("[.!?]");
+
+		// Check each sentence for the presence of the keyword
+		for (String sentence : sentenceArray) {
+			if (containsKeyword(sentence, keyword)) {
+				sentences.add(sentence.trim()); // trim() removes leading and trailing whitespaces
+			}
+		}
+
+		return sentences;
+	}
+
+	private static boolean containsKeyword(String sentence, String keyword) {
+		// Using a case-insensitive search for the keyword in the sentence
+		Pattern pattern = Pattern.compile("\\b" + Pattern.quote(keyword) + "\\b", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(sentence);
+
+		return matcher.find();
+	}
+
+	public static boolean containsDateInformation(String input, boolean getDates) {
+		if (getDates) {
+			// Regular expressions for weekdays, months, and years
+			String weekdaysRegex = "\\b(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\\b";
+			String monthsRegex = "\\b(January|February|March|April|May|June|July|August|September|October|November|December)\\b";
+			String yearsRegex = "\\b(\\d{4})\\b";
+
+			// Combine the regex patterns
+			String combinedRegex = String.join("|", weekdaysRegex, monthsRegex, yearsRegex);
+
+			// Create a pattern and matcher
+			Pattern pattern = Pattern.compile(combinedRegex);
+			Matcher matcher = pattern.matcher(input);
+
+			// Return true if any match is found
+			return matcher.find();
+		}
+		return false;
+	}
+
+	public static ArrayList<String> sortYearsAndMonths(ArrayList<String> stringsToBeSortedByDate) {
+		// Create a custom comparator for sorting
+        Comparator<String> yearComparator = Comparator.comparingInt(ScraperApp::extractYear)
+                .thenComparing(Comparator.naturalOrder());
+
+        // Sort the list using the custom comparator
+        stringsToBeSortedByDate.sort(yearComparator);
+
+        return stringsToBeSortedByDate;
+    }
+
+    private static Integer extractYear(String input) {
+    	Pattern yearPattern = Pattern.compile("\\b(\\d{4})\\b");
+        Matcher matcher = yearPattern.matcher(input);
+
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group());
+        }
+
+        // Return a large value for strings without a year
+        return Integer.MAX_VALUE;
+    }
+	
+}
---
+++ WikiScrape/WikiScrape/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author marjs
+ *
+ */
+module WikiScrape {
+	requires java.desktop;
+	requires org.pushingpixels.radiance.substance;
+	requires org.jsoup;
+}
